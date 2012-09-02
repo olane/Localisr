9547ac0061acd312a4320fdcadfc8bc5badd5b10
Giles Lavelle <giles.lavelle@gmail.com>
2012-09-02 14:56:20 +0100

    implemented a basic currency converter

--- extension/test.js
+++ extension/test.js
@@ -1,25 +1,67 @@
-// var s = document.createElement('script');
-
-var isTime = function(s){
-	return s.match(/[0-9]{2}:[0-9]{2}/);
+fx.base = "USD";
+fx.rates = {
+	"EUR" : 0.745101,
+	"GBP" : 0.647710,
+	"HKD" : 7.781919,
+	"USD" : 1
 };
 
-var words = jQuery('body').html().split(' ');
+var isTime = function(s){
+	return s.match(/^[0-9]{1,2}:[0-9]{2}$/);
+};
 
-// console.log(words);
-for (var i = 0; i < words.length; i++) {
-	if(isTime(words[i])){
-		var temp = jQuery('<div>');
-		var wrapper = jQuery('<span>');
-		wrapper.css({color: 'red'});
-		wrapper.text(words[i]);
-		temp.html(wrapper);
-		words[i] = temp.html();
+var isCurrency = function(s){
+	return s.match(/^(£|\$){1}[0-9]+(\.[0-9]{2})?$/);
+};
+
+var convertPrice = function(s){
+	var type = s[0];
+	var acronymn;
+	switch(type){
+		case '£':
+			acronymn = 'GBP';
+			break;
+		case '$':
+			acronymn = 'USD';
+			break;
 	}
-	// else {
-	// 	words[i] =
-	// }
-}
 
+	var price = parseFloat(s.substring(1, s.length));
+	var newPrice = fx.convert(price, {from: acronymn, to: 'USD'});
+	var newPriceString = '$' + newPrice.toFixed(2);
+	console.log(price);
+	return newPriceString;
+};
 
-jQuery('body').html(words.join(' '));
+var scan = function(element){
+	$(element).contents().each(function(index){
+		if(this.nodeType == 3){
+			var text = this.textContent;
+			words = text.split(' ');
+			var containsCurrency = false;
+			for (var i = 0; i < words.length; i++) {
+				if(isCurrency(words[i])){
+					containsCurrency = true;
+					words[i] = convertPrice(words[i]);
+
+					var temp = $('<div>');
+					var wrapper = $('<span>')
+						.css({color: 'red'})
+						.text(words[i])
+						.addClass('converted-price');
+					temp.html(wrapper);
+					words[i] = temp.html();
+				}
+			}
+			if(containsCurrency){
+				$(this).parent().append(words.join(' '));
+				$(this).remove();
+			}
+		}
+		else if(this.nodeType == 1){
+			scan(this);
+		}
+	});
+};
+
+scan('body');
