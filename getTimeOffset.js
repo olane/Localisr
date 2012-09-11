fdec33905a73b59feb8e402095615e212954c944
Oli Lane <olil4ne@gmail.com>
2012-09-11 14:48:35 +0100

    Added new method for converting times (WIP)

--- getTimeOffset.js
+++ getTimeOffset.js
@@ -1,11 +1,39 @@
 
-function getTimeOffset(){}
-	var rightNow = new Date();
-	var jan1 = new Date(rightNow.getFullYear(), rightNow.getMonth(), rightNow.getDate(), 0, 0, 0, 0);
-	var temp = jan1.toUTCString();
-	var jan2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-	var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
+function getTimeOffset(){
 
-	return std_time_offset;
+	var rightNow = new Date();
+
+	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+	var year = rightNow.getFullYear();
+	var month = rightNow.getMonth();
+	var date =  rightNow.getDate();
+	var hours24 = "20";
+	var minutes = "00";
+	var zone = "CST";
+
+	//Get a date object for the input time/date as UTC
+	var inputTimeUTCString = months[month] + " " + date + ", " + year + " " + hours24 + ":" + minutes + " " + zone;
+	var inputTimeDate = new Date(inputTimeUTCString);
+	    
+
+	//get a date object for midnight today in this timezone
+	var thisZone = new Date(rightNow.getFullYear(), rightNow.getMonth(), rightNow.getDate(), 0, 0, 0, 0);
+	//get that time as a UTC string
+	var thisZoneUTCString = thisZone.toUTCString();
+	//get a new date object for the UTC time, but in this timezone
+	var UTCZone = new Date(thisZoneUTCString.substring(0, thisZoneUTCString.lastIndexOf(" ")-1));
+	//difference between date objects will be the offset from UTC in this timezone (this time zone = UTC + -offset)
+	var time_offset = (thisZone - UTCZone) / (1000 * 60 * 60);
+
+	//add offset to input time (UTC version)
+	inputTimeDate.setTime(inputTimeDate.getTime() + time_offset * 60 * 60 * 1000);
+
+	//remove timezone from time with added offset
+	var newDateString = inputTimeDate.toUTCString().substring(0, inputTimeDate.toUTCString().lastIndexOf(" ")-3);
+	//make a final object with the converted time in
+	var newDate = new Date(newDateString);
+
+	return newDate.toString();
 }
 ​
