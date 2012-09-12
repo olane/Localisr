2892f827b70db8dabafd095369e643544ff33015
Giles Lavelle <giles.lavelle@gmail.com>
2012-09-12 21:26:46 +0100

    update grunt config

--- grunt.js
+++ grunt.js
@@ -1,65 +1,69 @@
 /*global module:false*/
 module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        meta: {
+            version: '0.1.2',
+            banner: '/*! Localisr - v<%= meta.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* http://github.com/IneffablePigeon/Localisr\n' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+            'Giles Lavelle, Oliver Lane; Licensed MIT */'
+        },
+        lint: {
+            files: ['extension/localisr.js']
+        },
+        qunit: {
+            files: []
+        },
+        concat: {
+            dist: {
+                src: [
+                    'src/intro.js',
+                    'src/time.js', 'src/price.js',
+                    'src/main.js',
+                    'src/outro.js'
+                ],
+                dest: 'extension/localisr.js'
+            }
+        },
+        min: {
+            dist: {
+            src: ['<banner>', 'extension/localisr.js'],
+            dest: 'extension/localisr.min.js'
+            }
+        },
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'lint qunit'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true
+            },
+            globals: {
+                jQuery: true,
+                '$': true,
+                accounting: true,
+                money: true,
+                Localisr: true,
+                chrome: true,
+                'console': true
+            }
+        },
+        uglify: {}
+    });
 
-  // Project configuration.
-  grunt.initConfig({
-    meta: {
-      version: '0.1.0',
-      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '* http://PROJECT_WEBSITE/\n' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-        'YOUR_NAME; Licensed MIT */'
-    },
-    lint: {
-      files: []
-    },
-    qunit: {
-      files: []
-    },
-    concat: {
-      dist: {
-        src: [
-          'src/intro.js',
-          'src/time.js', 'src/price.js',
-          'src/main.js',
-          'src/outro.js'
-        ],
-        dest: 'extension/localisr.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['extension/localisr.js'],
-        dest: 'extension/localisr.min.js'
-      }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
-
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
-
+    // Default task.
+    grunt.registerTask('default', 'concat lint min');
 };
